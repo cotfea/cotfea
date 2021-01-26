8914e19eaca0a4f61fc42e0000a4cd94f910316f
FooTearth <footearth@gmail.com>
2021-01-26 17:31:01 +0800

    color

--- src/patterns/color.js
+++ src/patterns/color.js
@@ -1,28 +1,73 @@
 const color = {
 
   // comment
+  //  - comment.block.coffee
+  //  - comment.line.number-sign.coffee
   comment: 'comment.block.coffee'
 
-  // boolean
-  // regex
+  // language - constant.language.coffee
   // constant
-  // null
-  // numeric ( hex octal binary decimal )
+  // null - constant.language.null.coffee
+  // boolean
+  //  - constant.language.boolean.false.coffee
+  //  - constant.language.boolean.true.coffee
   // char
+  // regex
+  // numeric
+  //    binary - constant.numeric.binary.coffee
+  //    decimal - constant.numeric.decimal.coffee
+  //    hex - constant.numeric.hex.coffee
+  //    octal - constant.numeric.octal.coffee
 , constant: 'constant.language.coffee'
 
-  // control
-  // operateor
+  // control - keyword.control.coffee
+  // operateor - keyword.operator.coffee
 , operator: 'keyword.operator.coffee'
 
+  // array
+  // object
 , arguments: 'meta.arguments.coffee'
 
+  // bracket
+  //  - punctuation.definition.parameters.begin.bracket.round.coffee
+  //  - punctuation.definition.parameters.end.bracket.round.coffee
 , bracket: 'punctuation.definition.parameters.begin.bracket.round.coffee'
 
-, string: 'string.quoted.single.coffee'
+  // string
+  //  - punctuation.definition.string.begin.coffee
+  //  - punctuation.definition.string.end.coffee
+  //  - string.quoted.script.coffee
+  //  - string.quoted.single.coffee
+  //  - string.quoted.double.coffee
+  //  - string.quoted.single.heredoc.coffee
+  //  - string.quoted.double.heredoc.coffee
+, string: 'string.quoted.script.coffee'
 
+  // variable
+  //  - super 
+  //  - this
+  //  - arguments
 , variable: 'variable.language.coffee'
+
+  // function
+  // console
 , function: 'support.function.coffee'
+
+  // support
+  //  class - support.class.coffee
+  //  variable - support.variable.coffee
+, support: 'support.class.coffee'
+
+  // storage
+  //  - storage.type.annotation.coffee
+  //  - storage.type.class.coffee
+  //  - storage.type.function.coffee
+, storage: 'storage.type.function.coffee'
+
+  // invalid
+  // illegal
+  // identifier
+, error: 'invalid.illegal.identifier.coffee'
 }
 
 export default color
