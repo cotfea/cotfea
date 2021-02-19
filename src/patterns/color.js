f7599d4f622c865ad5828206f2a9c8bd75b068a4
FooTearth <footearth@gmail.com>
2021-02-19 16:48:30 +0800

    20210219_1

--- src/patterns/color.js
+++ src/patterns/color.js
@@ -1,96 +1,54 @@
 const color = {
 
-  // comment
-  //  - comment.block.coffee
-  //  - comment.line.number-sign.coffee
-  // 注释
+  /* 
+   * [comment] [string] [keyword] [number] [regexp] [operator] [namespace]
+   * [type] [struct] [class] [interface] [enum] [typeParameter] [function]
+   * [method] [macro] [variable] [parameter] [property] [label]
+  */
+
+  // comment.block.coffee
+  // comment.line.number-sign.coffee
   comment: 'comment.block.coffee'
 
-  // language - constant.language.coffee
-  // constant
-  // null - constant.language.null.coffee
-  // boolean
-  //  - constant.language.boolean.false.coffee
-  //  - constant.language.boolean.true.coffee
-  // char
-  // regex
-  // numeric
-  //    binary - constant.numeric.binary.coffee
-  //    decimal - constant.numeric.decimal.coffee
-  //    hex - constant.numeric.hex.coffee
-  //    octal - constant.numeric.octal.coffee
-  // 常量
-, constant: 'constant.language.coffee'
-
-  // control - keyword.control.coffee
-  // operateor - keyword.operator.coffee
-  // 操作符 控制结构
-, operator: 'keyword.control.coffee'
-
-  // array
-  // object
-  // function
-  // 函数调用
-, funcCall: 'meta.function-call.coffee'
-
-  // bracket
-  //  - punctuation.definition.parameters.begin.bracket.round.coffee
-  //  - punctuation.definition.parameters.end.bracket.round.coffee
-  // 括号
 , bracket: 'storage.type.annotation.coffee'
-
-  // variable
-  //  - super 
-  //  - this
-  //  - arguments
-  //  - extends
-  // 内置关键字
-, inKeyword: 'variable.language.coffee'
-
-  // function
-  // console
-  // 内置对象及其函数调用
-, inObjFunc: 'support.function.console.coffee'
-
-  // support
-  //  class - support.class.coffee
-  //  variable - support.variable.coffee
-  // 内置关键字
-, class: 'support.variable.coffee'
-
-  // storage
-  //  - storage.type.annotation.coffee
-  //  - storage.type.class.coffee
-  //  - storage.type.function.coffee
-  // 类型类
-, klass: 'storage.type.function.coffee'
-
-  // invalid
-  // illegal
-  // identifier
-, error: 'invalid.illegal.identifier.coffee'
-
-  // string
-  //  - punctuation.definition.string.begin.coffee
-  //  - punctuation.definition.string.end.coffee
-  //  - string.quoted.script.coffee
-  //  - string.quoted.single.coffee
-  //  - string.quoted.double.coffee
-  //  - string.quoted.single.heredoc.coffee
-  //  - string.quoted.double.heredoc.coffee
-, string: 'string.quoted.script.coffee'
-
-  // constant.numeric.binary.coffee
-  // constant.numeric.decimal.coffee
-  // constant.numeric.hex.coffee
-  // constant.numeric.octal.coffee
-, numeric: 'constant.numeric.decimal.coffee'
 
   // constant.language.boolean.false.coffee
   // constant.language.boolean.true.coffee
 , boolean: 'constant.language.boolean.true.coffee'
-
 , null: 'constant.language.null.coffee'
+
+, string: 'string.quoted.script.coffee'
+, number: 'constant.numeric.decimal.coffee'
+, regexp: ''
+
+, variable: ''
+, constant: 'constant.language.coffee'
+, operator: 'keyword.control.coffee'
+, keyword: ''
+
+, namespace: ''
+, label: ''
+
+, enum: ''
+, type: ''
+, typeParameter: ''
+
+, struct: ''
+, class: 'support.variable.coffee'
+// , klass: 'storage.type.function.coffee'
+, interface: ''
+
+, function: ''
+, method: ''
+, macro: ''
+, parameter: ''
+, property: ''
+, funcCall: 'meta.function-call.coffee'
+
+, inKeyword: 'variable.language.coffee'
+, inObjFunc: 'support.function.console.coffee'
+
+, error: 'invalid.illegal.identifier.coffee'
 }
 
 export default color
